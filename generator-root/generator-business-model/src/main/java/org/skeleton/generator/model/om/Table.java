d542a96e903a1c83759858376785764c644679ac
Nicolas Thibault <nicolas.thibault@sklsft.org>
2014-01-23 14:24:26 +0100

    requetes oracle : get avec business keys

--- generator-root/generator-business-model/src/main/java/org/skeleton/generator/model/om/Table.java
+++ generator-root/generator-business-model/src/main/java/org/skeleton/generator/model/om/Table.java
@@ -1,7 +1,9 @@
 package org.skeleton.generator.model.om;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * representation of a table<br/>
@@ -85,4 +87,55 @@ public class Table {
 		return insertColumnList;
 
 	}
+	
+	public List<QualifiedColumn> getQualifiedColumns() {
+		
+		Set<String> tableAliases = new HashSet<String>();
+		String alias = this.name;
+		tableAliases.add(alias);
+		
+		List<QualifiedColumn> result = new ArrayList<QualifiedColumn>();
+		
+		for (int i = 1; i< columnList.size(); i++) {
+			if (columnList.get(i).referenceTable != null) {
+				result.add(getReferenceQualifiedColumn(alias, columnList.get(i), tableAliases));
+			} else {
+				result.add(new QualifiedColumn(this, alias, columnList.get(i)));
+			}
+		}
+		
+		return result;
+	}
+	
+	private QualifiedColumn getReferenceQualifiedColumn(String alias, Column column, Set<String> tableAliases) {
+		
+		QualifiedColumn qualifiedColumn = new QualifiedColumn(this, alias, column);
+		qualifiedColumn.children = column.referenceTable.getReferenceQualifiedChildren(qualifiedColumn, tableAliases);
+		
+		return qualifiedColumn;
+	}
+	
+	private List<QualifiedColumn> getReferenceQualifiedChildren(QualifiedColumn parent, Set<String> tableAliases) {
+		
+		List<QualifiedColumn> result = new ArrayList<QualifiedColumn>();
+		int j = 1;
+		while (tableAliases.contains(this.name + j)) {
+			j++;
+		}
+		String alias = this.name + j;
+		tableAliases.add(alias);
+		
+		for (int i = 1; i<= cardinality; i++) {
+			if (columnList.get(i).referenceTable != null) {
+				result.add(getReferenceQualifiedColumn(alias, columnList.get(i), tableAliases));
+			} else {
+				result.add(new QualifiedColumn(parent, this, alias, columnList.get(i)));
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	
 }
