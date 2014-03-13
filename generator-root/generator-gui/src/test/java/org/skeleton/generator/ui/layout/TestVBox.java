19270265279f758d85a70ac6f90be090f83f4f0f
Nicolas Thibault <nicolas.thibault@sklsft.org>
2014-03-13 18:15:33 +0100

    tests sur java fx et fxml en vue d'une ui

---
+++ generator-root/generator-gui/src/test/java/org/skeleton/generator/ui/layout/TestVBox.java
@@ -0,0 +1,25 @@
+package org.skeleton.generator.ui.layout;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class TestVBox extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Test");
+		AnchorPane anchorPane = (AnchorPane)FXMLLoader.load(getClass().getResource("/fxml/layout/test-vbox.fxml"));
+		Scene scene = new Scene(anchorPane);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
