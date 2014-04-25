d470aa74f97880f443f8357ca438d891dc6b6b35
Nicolas Thibault <nicolas.thibault@sklsft.org>
2014-04-25 16:44:31 +0200

    petite revue de la jaavdoc...

--- generator-root/generator-business-model/src/main/java/org/skeleton/generator/model/metadata/SkeletonType.java
+++ generator-root/generator-business-model/src/main/java/org/skeleton/generator/model/metadata/SkeletonType.java
@@ -4,8 +4,8 @@ import javax.xml.bind.annotation.XmlEnum;
 
 /**
  * For the current release, the following skeletons are supported :
- * <li>RichFaces web application with spring and hibernate
- * <li>A simple version of a RichFaces web application with spring and hibernate
+ * <li>SPRING_HIBERNATE_RICHFACES : RichFaces web application with spring and hibernate
+ * <li>BASIC_SPRING_HIBERNATE_RICHFACES : A simple version of a RichFaces web application with spring and hibernate
  * @author Nicolas Thibault
  *
  */
