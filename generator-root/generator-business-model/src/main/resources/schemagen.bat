42022ba02884cf59ac7dac38fb64f0dcd830310d
Nicolas Thibault <nicolas.thibault@sklsft.org>
2014-07-29 18:50:30 +0200

    revue des xsd correction de bug sur un check populate

--- generator-root/generator-business-model/src/main/resources/schemagen.bat
+++ generator-root/generator-business-model/src/main/resources/schemagen.bat
@@ -1,5 +1,5 @@
 %JAVA%\schemagen ..\java\org\sklsft\generator\model\backup\*.java
-del backup-1.0.xsd
-rename schema1.xsd backup-1.0.xsd
+del backup-1.1.xsd
+rename schema1.xsd backup-1.1.xsd
 
 pause
