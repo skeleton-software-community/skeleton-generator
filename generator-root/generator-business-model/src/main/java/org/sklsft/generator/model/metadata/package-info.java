affba4357ba32f3d008bf296097d95e5245b1ac7
Nicolas Thibault <nicolas.thibault@sklsft.org>
2014-07-30 10:45:31 +0200

    correction du logger correction des xsd correction du batch

--- generator-root/generator-business-model/src/main/java/org/sklsft/generator/model/metadata/package-info.java
+++ generator-root/generator-business-model/src/main/java/org/sklsft/generator/model/metadata/package-info.java
@@ -1,5 +1,5 @@
-@XmlSchema(namespace = "http://www.sklsft.org/generator/model/metadata",
-xmlns = {@XmlNs( prefix = "metadata", namespaceURI = "http://www.sklsft.org/generator/model/metadata") },
+@XmlSchema(namespace = "http://generator.sklsft.org/schema/model/metadata",
+xmlns = {@XmlNs( prefix = "metadata", namespaceURI = "http://generator.sklsft.org/schema/model/metadata") },
 elementFormDefault = javax.xml.bind.annotation.XmlNsForm.UNSET)
 package org.sklsft.generator.model.metadata;
 
