f2bf357901947fcebf9ea3f32f2f0224c86dd9be
Nicolas Thibault <nicolas.thibault@sklsft.org>
2016-03-14 23:35:04 +0100

    corrections du template rich 4

--- generator-root/maven-versions.bat
+++ generator-root/maven-versions.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p NEW_VERSION=Enter new version :
 
-call mvn versions:set -DnewVersion=%NEW_VERSION%
+call mvn versions:set -DnewVersion=%NEW_VERSION% -DgenerateBackupPoms=false
 
 pause
