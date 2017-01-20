f03db40cfa9ba98b9b6b1b831a5a28615aeb81bf
Nicolas Thibault <nicolas.thibault@sklsft.org>
2017-01-21 00:30:06 +0100

    add data injector

---
+++ generator-root/generator-repository/src/main/java/org/sklsft/generator/repository/backup/command/impl/JUnitDataInjector.java
@@ -0,0 +1,24 @@
+package org.sklsft.generator.repository.backup.command.impl;
+
+import javax.inject.Inject;
+
+import org.sklsft.generator.repository.backup.command.interfaces.BackupArgumentsCommand;
+import org.sklsft.generator.repository.backup.reader.impl.TextDelimitedFileBackupReader;
+import org.sklsft.generator.repository.backup.reader.interfaces.BackupArgumentReader;
+import org.sklsft.generator.repository.backup.reader.model.BackupArguments;
+
+
+public class JUnitDataInjector {
+	
+	@Inject
+	private BackupArgumentsCommandFactory commandFactory;
+		
+	
+	public void inject(String filePath, Class<?> commandClass) {
+		BackupArgumentReader argumentReader = new TextDelimitedFileBackupReader();
+		BackupArguments commandArgs = argumentReader.readBackupArgs(filePath);
+		
+		BackupArgumentsCommand command = commandFactory.getCommand(commandClass);
+		command.execute(commandArgs);
+	}
+}
