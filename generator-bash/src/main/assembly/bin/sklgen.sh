b5d4c0c4ba6897243b029405d1a9944ac22267e4
Nicolas Thibault <nicolas.thibault@sklsft.org>
2023-03-06 14:32:15 +0100

    prepare next release

--- generator-bash/src/main/assembly/bin/sklgen.sh
+++ generator-bash/src/main/assembly/bin/sklgen.sh
@@ -6,7 +6,7 @@
 ###############################################################################
 
 
-SKLGEN_VERSION=4.0.0
+SKLGEN_VERSION=4.1.0-SNAPSHOT
 SKLGEN_RUNNABLE_JAR="$SKLGEN_HOME/boot/generator-bash-$SKLGEN_VERSION.jar"
 SKLGEN_LIB=$SKLGEN_HOME/lib
 SKLGEN_CLASSPATH=$SKLGEN_RUNNABLE_JAR:$SKLGEN_LIB/*
